58a35a6c58ff408150b860488b0c76a7ddedbd7f
Mithun021 <mithunkr79038@gmail.com>
2025-04-01 00:17:24 +0530

    checking

--- app/Config/App.php
+++ app/Config/App.php
@@ -12,27 +12,15 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      *
      * URL to your CodeIgniter root. Typically, this will be your base URL,
-     * WITH a trailing slash
+     * WITH a trailing slash:
      *
      * E.g., http://example.com/
      */
     public string $baseURL = 'https://dummy.agomps.com/';
 
-    public $sessionDriver = 'CodeIgniter\Session\Handlers\FileHandler';  // Default session handler
-    public $sessionCookieName = 'ci_session';  // The name of your session cookie
-    public $sessionExpiration = 0;  // Session expiry in seconds (2 hours here)
-    public $sessionMatchIP = false;
-
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
-     *
-     * E.g.,
-     * When your site URL ($baseURL) is 'http://example.com/', and your site
-     * also accepts 'http://media.example.com/' and 'http://accounts.example.com/':
-     *     ['media.example.com', 'accounts.example.com']
-     *
-     * @var list<string>
      */
     public array $allowedHostnames = [];
 
@@ -53,50 +41,23 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      *
      * This item determines which server global should be used to retrieve the
-     * URI string. The default setting of 'REQUEST_URI' works for most servers.
-     * If your links do not seem to work, try one of the other delicious flavors:
-     *
-     *  'REQUEST_URI': Uses $_SERVER['REQUEST_URI']
-     * 'QUERY_STRING': Uses $_SERVER['QUERY_STRING']
-     *    'PATH_INFO': Uses $_SERVER['PATH_INFO']
-     *
-     * WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
+     * URI string.
      */
     public string $uriProtocol = 'REQUEST_URI';
 
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed URL Characters
-    |--------------------------------------------------------------------------
-    |
-    | This lets you specify which characters are permitted within your URLs.
-    | When someone tries to submit a URL with disallowed characters they will
-    | get a warning message.
-    |
-    | As a security measure you are STRONGLY encouraged to restrict URLs to
-    | as few characters as possible.
-    |
-    | By default, only these are allowed: `a-z 0-9~%.:_-`
-    |
-    | Set an empty string to allow all characters -- but only if you are insane.
-    |
-    | The configured value is actually a regular expression character group
-    | and it will be used as: '/\A[<permittedURIChars>]+\z/iu'
-    |
-    | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
-    |
-    */
+    /**
+     * --------------------------------------------------------------------------
+     * Allowed URL Characters
+     * --------------------------------------------------------------------------
+     *
+     * This lets you specify which characters are permitted within your URLs.
+     */
     public string $permittedURIChars = 'a-z 0-9~%.:_\-';
 
     /**
      * --------------------------------------------------------------------------
      * Default Locale
      * --------------------------------------------------------------------------
-     *
-     * The Locale roughly represents the language and location that your visitor
-     * is viewing the site from. It affects the language strings and other
-     * strings (like currency markers, numbers, etc), that your program
-     * should run under for this request.
      */
     public string $defaultLocale = 'en';
 
@@ -107,8 +68,6 @@ class App extends BaseConfig
      *
      * If true, the current Request object will automatically determine the
      * language to use based on the value of the Accept-Language header.
-     *
-     * If false, no automatic detection will be performed.
      */
     public bool $negotiateLocale = false;
 
@@ -116,14 +75,6 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Supported Locales
      * --------------------------------------------------------------------------
-     *
-     * If $negotiateLocale is true, this array lists the locales supported
-     * by the application in descending order of priority. If no match is
-     * found, the first locale will be used.
-     *
-     * IncomingRequest::setLocale() also uses this list.
-     *
-     * @var list<string>
      */
     public array $supportedLocales = ['en'];
 
@@ -131,12 +82,6 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Application Timezone
      * --------------------------------------------------------------------------
-     *
-     * The default timezone that will be used in your application to display
-     * dates with the date helper, and can be retrieved through app_timezone()
-     *
-     * @see https://www.php.net/manual/en/timezones.php for list of timezones
-     *      supported by PHP.
      */
     public string $appTimezone = 'UTC';
 
@@ -144,11 +89,6 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Default Character Set
      * --------------------------------------------------------------------------
-     *
-     * This determines which character set is used by default in various methods
-     * that require a character set to be provided.
-     *
-     * @see http://php.net/htmlspecialchars for a list of supported charsets.
      */
     public string $charset = 'UTF-8';
 
@@ -156,11 +96,6 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Force Global Secure Requests
      * --------------------------------------------------------------------------
-     *
-     * If true, this will force every request made to this application to be
-     * made via a secure connection (HTTPS). If the incoming request is not
-     * secure, the user will be redirected to a secure version of the page
-     * and the HTTP Strict Transport Security (HSTS) header will be set.
      */
     public bool $forceGlobalSecureRequests = false;
 
@@ -168,22 +103,6 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Reverse Proxy IPs
      * --------------------------------------------------------------------------
-     *
-     * If your server is behind a reverse proxy, you must whitelist the proxy
-     * IP addresses from which CodeIgniter should trust headers such as
-     * X-Forwarded-For or Client-IP in order to properly identify
-     * the visitor's IP address.
-     *
-     * You need to set a proxy IP address or IP address with subnets and
-     * the HTTP header for the client IP address.
-     *
-     * Here are some examples:
-     *     [
-     *         '10.0.1.200'     => 'X-Forwarded-For',
-     *         '192.168.5.0/24' => 'X-Real-IP',
-     *     ]
-     *
-     * @var array<string, string>
      */
     public array $proxyIPs = [];
 
@@ -191,17 +110,31 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Content Security Policy
      * --------------------------------------------------------------------------
-     *
-     * Enables the Response's Content Secure Policy to restrict the sources that
-     * can be used for images, scripts, CSS files, audio, video, etc. If enabled,
-     * the Response object will populate default values for the policy from the
-     * `ContentSecurityPolicy.php` file. Controllers can always add to those
-     * restrictions at run time.
-     *
-     * For a better understanding of CSP, see these documents:
-     *
-     * @see http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-     * @see http://www.w3.org/TR/CSP/
      */
     public bool $CSPEnabled = false;
+
+    /**
+     * --------------------------------------------------------------------------
+     * Session Configurations
+     * --------------------------------------------------------------------------
+     *
+     * Session configuration for the app.
+     */
+    public string $sessionDriver = 'CodeIgniter\Session\Handlers\FileHandler';  // Default session handler
+    public string $sessionCookieName = 'ci_session';  // The name of your session cookie
+    public int $sessionExpiration = 0;  // Session expiration in seconds (0 means until browser is closed)
+    public bool $sessionMatchIP = false;  // Whether the session should match the IP address (set to false if not needed)
+
+    /**
+     * --------------------------------------------------------------------------
+     * Cookie Configuration
+     * --------------------------------------------------------------------------
+     *
+     * Cookie configuration for your app.
+     */
+    public bool $cookieSecure = true;  // Ensures cookies are only sent over HTTPS connections (set to false for testing purposes if needed)
+    public string $cookiePath = '/';  // Path on the server where the cookie will be available
+    public string $cookieDomain = '';  // Set domain if you're using subdomains
+    public string $cookieSameSite = 'Lax';  // Options: 'None', 'Lax', 'Strict' (None requires Secure to be true)
+
 }
