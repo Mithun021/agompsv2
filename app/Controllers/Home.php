fe57d659435e63a5100a54b58f45baff7a641b01
Mithun021 <mithunkr79038@gmail.com>
2025-04-03 17:54:49 +0530

    Login Register

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index(): string
     {
+        if(session()->get('isLoggedIn')){
+            echo session()->get('customer_ac_id'); die;
+        }
+
         $data = ['title' => 'Agomps India'];
         return view('index',$data);
     }
