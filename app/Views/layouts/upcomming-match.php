4e36bc71e594d186a49850b2ef4603d9a14b0023
Mithun021 <mithunkr79038@gmail.com>
2025-04-06 16:35:56 +0530

    filter tournament

--- app/Views/layouts/upcomming-match.php
+++ app/Views/layouts/upcomming-match.php
@@ -18,18 +18,10 @@
     </div>
     <div class="col-md-4 p-1">
         <div class="card card-body p-2 mb-0">
-            <a href="#" target="_blank">
-            <div class="teams-name">
-                <img src="<?= base_url() ?>public/assets/images/teams/team1.png" alt="" srcset="">
-                <span>V/S</span>
-                <img src="<?= base_url() ?>public/assets/images/teams/team2.png" alt="" srcset="">
-            </div>
-            <hr class="my-1">
-            <p class="my-2 text-center">Date : 12/APR/2025 6:10 PM</p>
-            </a>
+            <h1 class="text-center py-5">ANNOUNCED COMMING SOON</h1>
         </div>
     </div>
-    <div class="col-md-4 p-1">
+    <!-- <div class="col-md-4 p-1">
         <div class="card card-body p-2 mb-0">
             <a href="#" target="_blank">
             <div class="teams-name">
@@ -41,18 +33,5 @@
             <p class="my-2 text-center">Date : 12/APR/2025 6:10 PM</p>
             </a>
         </div>
-    </div>
-    <div class="col-md-4 p-1">
-        <div class="card card-body p-2 mb-0">
-            <a href="#" target="_blank">
-            <div class="teams-name">
-                <img src="<?= base_url() ?>public/assets/images/teams/team1.png" alt="" srcset="">
-                <span>V/S</span>
-                <img src="<?= base_url() ?>public/assets/images/teams/team2.png" alt="" srcset="">
-            </div>
-            <hr class="my-1">
-            <p class="my-2 text-center">Date : 12/APR/2025 6:10 PM</p>
-            </a>
-        </div>
-    </div>
+    </div> -->
 </dv>
