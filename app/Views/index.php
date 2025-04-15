369d63a7327ffc78eae81e4581cf88c938e5fce9
Mithun021 <mithunkr79038@gmail.com>
2025-04-15 10:54:45 +0530

    first commit

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layouts/master') ?>
 <?= $this->section('body-content') ?>
 <?= view('layouts/banner-slider') ?>
-
+<?= view('layouts/tournament-start-countdown') ?>
 <div class="container-fluid">
     <?= view('layouts/tournament-category') ?>
     <?= view('layouts/tournaments') ?>
---
+++ app/Views/layouts/tournament-start-countdown.php
@@ -0,0 +1,12 @@
+<section id="countdown-section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 text-center">
+                <h1 class="mt-5">Tournament Start Countdown</h1>
+                <div id="countdown" class="mt-4"></div>
+            </div>
+            <div class="col-6 text-center">
+            </div>
+        </div>
+    </div>
+</section>
