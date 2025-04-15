9e32a7aba9dd26c04f3723497e1ff5ec6e586700
Mithun021 <mithunkr79038@gmail.com>
2025-04-15 23:20:27 +0530

    first commit

--- app/Views/layouts/tournament-start-countdown.php
+++ app/Views/layouts/tournament-start-countdown.php
@@ -2,12 +2,36 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-6 text-center">
-                <h1 class="mt-5">Tournament Start Countdown</h1>
-                <div id="countdown" class="mt-4"></div>
+                <h1 class="mt-5">Tournament Start</h1>
+                <div class="counter" id="countdown">Loading...</div>
             </div>
             <div class="col-6 text-center">
-                <img src="<?= base_url() ?>public/assets/images/UPPL.jpg" alt="">
+                <img src="<?= base_url() ?>public/assets/images/UPPL.jpg" alt="" width="100%">
             </div>
         </div>
     </div>
 </section>
+<script>
+    // Set the target date to 17 April 2025 at 3:00 PM
+    const targetDate = new Date("April 17, 2025 15:00:00").getTime();
+
+    const countdown = document.getElementById("countdown");
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      if (distance < 0) {
+        clearInterval(interval);
+        countdown.innerHTML = "🎉 Time's up!";
+        return;
+      }
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }, 1000);
+  </script>
