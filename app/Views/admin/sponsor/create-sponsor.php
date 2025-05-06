d6e470cd1b78f8f48ae0524d2495ad04680e9842
Mithun021 <mithunkr79038@gmail.com>
2025-05-06 15:24:18 +0530

    first commit

--- app/Views/admin/sponsor/create-sponsor.php
+++ app/Views/admin/sponsor/create-sponsor.php
@@ -26,6 +26,43 @@
                                 <?php } ?>
                                 </select>
                             </div>
+                            <div class="col-md-4 form-group">
+                                <span>Package Name</span>
+                                <select name="package_name" id="package_name" class="form-control form-control-sm">
+                                    <option value="">--Select--</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <span>Package Type</span>
+                                <select name="package_type" id="package_type" class="form-control form-control-sm">
+                                    <option value="">--Select--</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <span>Promotion Days</span>
+                                <select name="promotion_days" class="form-control form-control-sm">
+                                    <option value="">--Select--</option>
+                                    <?php for ($i=0; $i <= 30 ; $i++) { 
+                                        echo '<option value="'.$i.'">'.$i.'</option>';
+                                    } ?>
+                                    <option value="Parmanent">Parmanent</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <span>Promotion Location</span>
+                                <select name="promotion_location" id="promotion_location" class="form-control form-control-sm">
+                                    <option value="">--Select--</option>
+                                    <option value="Home (Footer)">Home (Footer)</option>
+                                    <option value="Side Panel">Side Panel</option>
+                                    <option value="Top Slider">Top Slider</option>
+                                    <option value="Sponsors Section">Sponsors Section</option>
+                                    <option value="Blog/News">Blog/News</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4 form-group">
+                                <span>Price (INR)</span>
+                                <input type="number" class="promotion_amount" class="form-control form-control-sm">
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer p-2">
@@ -36,5 +73,14 @@
         </div>
     </div>
 </div>
+
+<!-- jQuery  -->
+<script src="<?= base_url() ?>public/admin/assets/js/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        
+    });
+</script>
+
 
 <?= $this->endSection() ?>
