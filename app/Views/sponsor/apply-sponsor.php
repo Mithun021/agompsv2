288e9f84382b9c4cef0f1e8140615dabc84d22dd
Mithun021 <mithunkr79038@gmail.com>
2025-05-06 19:31:05 +0530

    first commit

--- app/Views/sponsor/apply-sponsor.php
+++ app/Views/sponsor/apply-sponsor.php
@@ -8,43 +8,51 @@
                 <form action="<?= base_url() ?>apply-sponsor/<?= $sponsor_id ?>" method="post">
                     <div class="row">
                         <div class="col-md-12 form-group">
-                            <span>Firm/Business/Company Name</span>
+                            <span>Firm/Business/Company Name<span class="text-danger">*</span></span>
                             <input type="text" class="form-control form-control-sm" name="firm_name" required>
                         </div>
+                        <div class="col-md-6 form-group">
+                            <span>Firm/Business/Company Type<span class="text-danger">*</span></span>
+                            <input type="text" class="form-control form-control-sm" name="firm_type" required>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <span>Firm/Business/Company Logo</span>
+                            <input type="file" class="form-control form-control-sm" name="firm_logo">
+                        </div>
                         <div class="col-md-12 form-group">
-                            <span>Customer Name</span>
+                            <span>Customer Name<span class="text-danger">*</span></span>
                             <input type="text" class="form-control form-control-sm" name="customer_name" required>
                         </div>
                         <div class="col-md-6 form-group">
-                            <span>Customer Phone</span>
+                            <span>Customer Phone<span class="text-danger">*</span></span>
                             <input type="tel" class="form-control form-control-sm" name="phone_number" maxlength="10" required>
                         </div>
                         <div class="col-md-6 form-group">
-                            <span>Customer E-Mail</span>
+                            <span>Customer E-Mail<span class="text-danger">*</span></span>
                             <input type="email" class="form-control form-control-sm" name="email_id" required>
                         </div>
                         <div class="col-md-4 form-group">
-                            <span>State</span>
+                            <span>State<span class="text-danger">*</span></span>
                             <input type="email" class="form-control form-control-sm" name="state" required>
                         </div>
                         <div class="col-md-4 form-group">
-                            <span>City</span>
+                            <span>City<span class="text-danger">*</span></span>
                             <input type="email" class="form-control form-control-sm" name="city" required>
                         </div>
                         <div class="col-md-4 form-group">
-                            <span>Pincode</span>
+                            <span>Pincode<span class="text-danger">*</span></span>
                             <input type="email" class="form-control form-control-sm" name="pincode" required>
                         </div>
                         <div class="col-md-6 form-group">
-                            <span>Aadhar Number</span>
+                            <span>Aadhar Number<span class="text-danger">*</span></span>
                             <input type="number" class="form-control form-control-sm" name="aadhar_no" maxlength="12" required>
                         </div>
                         <div class="col-md-6 form-group">
-                            <span>PAN Number</span>
+                            <span>PAN Number<span class="text-danger">*</span></span>
                             <input type="text" class="form-control form-control-sm" name="pan_no" required>
                         </div>
                         <div class="col-md-12 form-group">
-                            <span>Amount</span>
+                            <span>Amount<span class="text-danger">*</span></span>
                             <input type="text" class="form-control form-control-sm" name="sponsor_amount" value="<?= $sponsor_detail['discount_promotion_amount'] ??  $sponsor_detail['promotion_amount'] ?>" readonly required>
                         </div>
                         <div class="col-md-12 form-group">
