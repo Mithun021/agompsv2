8486673af96fc4e309fb51d5fe9d1fd0b7238b03
Mithun021 <mithunkr79038@gmail.com>
2025-05-07 12:18:08 +0530

    first commit

--- app/Views/sponsor/apply-sponsor.php
+++ app/Views/sponsor/apply-sponsor.php
@@ -5,6 +5,11 @@
         <div class="col-lg-12">
             <div class="card card-body">
                 <h2 class="text-primary text-center">Apply Now</h2>
+                    <?php
+                        if (session()->getFlashdata('status')) {
+                            echo session()->getFlashdata('status');
+                        }
+                    ?>
                 <form action="<?= base_url() ?>apply-sponsor/<?= $sponsor_id ?>" method="post" enctype="multipart/form-data">
                     <div class="row">
                         <div class="col-md-12 form-group">
@@ -13,7 +18,7 @@
                         </div>
                         <div class="col-md-6 form-group">
                             <span>Firm/Business/Company Type<span class="text-danger">*</span></span>
-                            <input type="text" class="form-control form-control-sm" name="firm_type" required>
+                            <input type="text" class="form-control form-control-sm" name="firm_type" value="<?= $customer_detail['firm_type'] ?? '' ?>" <?php if($customer_detail['firm_type']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-6 form-group">
                             <span>Firm/Business/Company Logo</span>
@@ -21,42 +26,46 @@
                         </div>
                         <div class="col-md-12 form-group">
                             <span>Customer Name<span class="text-danger">*</span></span>
-                            <input type="text" class="form-control form-control-sm" name="customer_name" required>
+                            <input type="text" class="form-control form-control-sm" name="customer_name" value="<?= $customer_detail['customer_name'] ?? '' ?>" <?php if($customer_detail['customer_name']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-6 form-group">
                             <span>Customer Phone<span class="text-danger">*</span></span>
-                            <input type="tel" class="form-control form-control-sm" name="phone_number" maxlength="10" required>
+                            <input type="tel" class="form-control form-control-sm" name="phone_number" maxlength="10" value="<?= $customer_detail['phone_number'] ?? '' ?>" <?php if($customer_detail['phone_number']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-6 form-group">
                             <span>Customer E-Mail<span class="text-danger">*</span></span>
-                            <input type="email" class="form-control form-control-sm" name="email_id" required>
+                            <input type="email" class="form-control form-control-sm" name="email_id" value="<?= $customer_detail['email_id'] ?? '' ?>" <?php if($customer_detail['email_id']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-4 form-group">
                             <span>State<span class="text-danger">*</span></span>
-                            <input type="text" class="form-control form-control-sm" name="state" required>
+                            <input type="text" class="form-control form-control-sm" name="state" value="<?= $customer_detail['state'] ?? '' ?>" <?php if($customer_detail['state']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-4 form-group">
                             <span>City<span class="text-danger">*</span></span>
-                            <input type="text" class="form-control form-control-sm" name="city" required>
+                            <input type="text" class="form-control form-control-sm" name="city" value="<?= $customer_detail['city'] ?? '' ?>" <?php if($customer_detail['city']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-4 form-group">
                             <span>Pincode<span class="text-danger">*</span></span>
-                            <input type="number" class="form-control form-control-sm" name="pincode" maxlength="6" required>
+                            <input type="number" class="form-control form-control-sm" name="pincode" maxlength="6" value="<?= $customer_detail['pincode'] ?? '' ?>" <?php if($customer_detail['pincode']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-6 form-group">
                             <span>Aadhar Number<span class="text-danger">*</span></span>
-                            <input type="number" class="form-control form-control-sm" name="aadhar_no" maxlength="12" required>
+                            <input type="number" class="form-control form-control-sm" name="aadhar_no" maxlength="12" value="<?= $customer_detail['aadhar_no'] ?? '' ?>" <?php if($customer_detail['aadhar_no']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-6 form-group">
                             <span>PAN Number<span class="text-danger">*</span></span>
-                            <input type="text" class="form-control form-control-sm" name="pan_no" required>
+                            <input type="text" class="form-control form-control-sm" name="pan_no" value="<?= $customer_detail['pan_no'] ?? '' ?>" <?php if($customer_detail['pan_no']){ echo "readonly"; } ?> required>
                         </div>
                         <div class="col-md-12 form-group">
                             <span>Amount<span class="text-danger">*</span></span>
                             <input type="text" class="form-control form-control-sm" name="sponsor_amount" value="<?= $sponsor_detail['discount_promotion_amount'] ??  $sponsor_detail['promotion_amount'] ?>" readonly required>
                         </div>
                         <div class="col-md-12 form-group">
+                            <?php if($sponsor_detail){ ?>
                             <input type="submit" class="btn btn-primary" value="Apply Now">
+                            <?php }else { ?>
+                                <input type="submit" class="btn btn-primary" value="Pay Now" name="sponsor_payment_btn">
+                            <?php } ?>
                         </div>
                     </div>
                 </form>
